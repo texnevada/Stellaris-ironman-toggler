a8257d26d5a6dfd70c70d22cb1331f4209fdf29b
texnevada <15308761+texnevada@users.noreply.github.com>
2020-05-21 18:45:42 +0200

    Updated to version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # Stellaris-ironman-toggler
+
+This software is only tested on Windows 10 and may or may not work for other operating systems.
+
+###Disclaimer:
+This program was tested with a new empire that started with ironman and even after
+console edits and ironman reapplied. Achievements was still possible.
+I am however not responsible for any damages to your save game.
+While this program does back up the game for you.
+You should still back up the game before you attempt to use the program just in case.
+Use at your own risk. You have been warned.
+
+How does this work?
+=
+First you put the file in your folder where your save is located.
+The program will look for the `ironman.sav` file to enable or disable ironman.sav.
+The program will then goes through the entire save and looks for the ironman 
+references and turns them off or on.
+
+What is the purpose of this tool?
+=
+Have you ever had a game suddenly bug on you and you wish you could access the ingame
+console to fix it? Well with this you can!
+
+Does this mean I can turn my save into ironman and get achievements?
+=
+It is possible, though it is not the intended purpose of this tool.
+If your savegame was modded to any extent and you enable ironman.
+The game will not award you achievements due to the checksum verification being wrong.
+
+# How to use the program
+
+1. **Autosave is recommended to be turned off in the gameplay settings for this to work!
+You can re-enable this later**
+![](Autosave%20to%20Cloud%20Off.jpg)
+
+2. **When saving the game. You also have to make sure not to save to the cloud!
+Otherwise the game will not save a proper local version on your PC. (This is the case with
+the steam version. I have no knowledge of the other versions)**
+![](Do%20not%20save%20to%20the%20cloud.jpg)
+
+3. Once you have done the following above. Go to your save in-game and save your game.
+This should create a local save file in save games folder with your empire name.
+``~\Documents\Paradox Interactive\Stellaris\save games\YourEmpire\``
+
+4. Make sure your save is called ``ironman.sav`` otherwise the program won't enable/disable
+ironman for you. 
+**If you are turning a normal game save into ironman mode. You need to name your
+save** ``ironman.sav``.
+
+5. Run the program inside the save folder of your empire. It will let you know when it is done.
+
+6. If you just disabled the ironman mode. You should then now be able to boot up your game
+and do what you need to do in the console. Once you finish. Save the game and close out of it.
+Then proceed to use the program again. **Important:** Once it has re-enabled ironman mode. 
+You need to boot up your save and close it for it to take effect properly even though it
+looks like ironman is enabled!
--- StellarisIronmanToggler.py
+++ StellarisIronmanToggler.py
@@ -1,37 +1,113 @@
-
 # Imports
-
 from shutil import copyfile
 import zipfile
 from datetime import date, datetime
 import os
 
+"""
+=======
+Prep
+=======
+"""
+
 ZipFile = zipfile.ZipFile
 now = datetime.now()
-time = now.strftime("%H-%M-%S")
+timenow = now.strftime(f"%H-%M-%S-%f")
 
 """
 ========
 Code
 ========
 """
-try:
-    copyfile("./ironman.sav", f"./ironman_backup_{date.today()}_{time}.sav")
-except Exception as e:
-    print(e)
 
 
-try:
-    with ZipFile("ironman.sav", "r") as ZipFolder:
-        for x in ZipFolder.namelist():
-            print(x)
+def backup():
+    try:
+        print("[INFO]: Backing up your save.")
+        copyfile("./ironman.sav", f"./ironman_backup_{date.today()}_{timenow}.sav")
+        print("[INFO]: Backup complete.")
+        extraction()
+    except Exception as e:
+        print(f"[WARN]: {e}")
 
-except Exception as e:
-    print(e)
 
-""" 
-try:
-    pass
-except Exception as e:
-    print(e)
-"""
+def extraction():
+    try:
+        with ZipFile("ironman.sav", "r") as zipf:
+            zipf.extractall()
+            zipf.close()
+        os.remove("ironman.sav")
+        print("[INFO]: Extracted files from save")
+        save_edit()
+    except Exception as e:
+        print(f"[WARN]: {e}")
+
+
+def save_edit():
+    try:
+        print("[INFO]: Reading meta file & preparing changes")
+        file = open("meta", "r")
+        open("meta_new", "x")
+        file2 = open("meta_new", "a")
+        for line in file:
+            if "ironman=yes" in line:
+                print("[INFO]: Found ironman reference.")
+                print("[INFO]: Disabling ironman")
+                file2.write("ironman=no")
+                print("[INFO]: Finishing up meta changes")
+            elif "ironman=no" in line:
+                print("[INFO]: Found ironman reference.")
+                print("[INFO]: Enabling ironman")
+                file2.write("ironman=yes")
+                print("[INFO]: Finishing up meta changes")
+            else:
+                file2.write(line)
+        file.close()
+        file2.close()
+        print("[INFO]: Finished the new meta file")
+
+        print("[INFO]: Reading gamestate file & preparing changes")
+        file = open("gamestate", "r")
+        open("gamestate_new", "x")
+        file2 = open("gamestate_new", "a")
+        for line in file:
+            if "ironman=yes" in line:
+                print("[INFO]: Found ironman reference.")
+                print("[INFO]: Disabling ironman")
+                file2.write("	ironman=no\n")
+            elif "ironman=no" in line:
+                print("[INFO]: Found ironman reference.")
+                print("[INFO]: Enabling ironman")
+                file2.write("	ironman=yes\n")
+            else:
+                file2.write(line)
+        file.close()
+        file2.close()
+        print("[INFO]: Finished the new gamestate file")
+
+        os.remove("meta")
+        os.rename("meta_new", "meta")
+
+        os.remove("gamestate")
+        os.rename("gamestate_new", "gamestate")
+        insertion()
+    except Exception as e:
+        print(f"[WARN]: {e}")
+
+
+def insertion():
+    try:
+        with ZipFile("ironman.sav", "a") as ZipFolder:
+            print("[INFO]: Adding new meta file to ironman save")
+            ZipFolder.write("meta")
+            print("[INFO]: Adding new gamestate file to ironman save")
+            ZipFolder.write("gamestate")
+            ZipFolder.close()
+        os.remove("meta")
+        os.remove("gamestate")
+        input("[INFO]: DONE. PRESS ENTER TO EXIT PROGRAM")
+    except Exception as e:
+        print(f"[WARN]: {e}")
+
+
+backup()
